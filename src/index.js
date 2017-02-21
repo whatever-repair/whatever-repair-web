d71ef42875bad442ba4adcd02e56b52b33e34435
JinHo Do <greatdjh@gmail.com>
2017-02-21 23:06:52 +0900

    i can do it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-// 여기서 html에 렌더
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
