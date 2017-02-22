c0a1fd6d49b3236d0d041cf94de6bb5cc63659f8
hyojeong Son <web.sonhyoj@gmail.com>
2017-02-22 18:08:47 +0900

    src file path uplode

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # whatever-repair-web
 :hammer_and_pick: 수리견적 서비스입니다. 
-
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (
-      <h1>뭐든지 수리!!!</h1>
+      <h1>뭐든지 수리!</h1>
     );
   };
 }
