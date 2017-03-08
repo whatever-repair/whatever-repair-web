7554edc60b7df7ae25706beb60b2526f2d127f1d
hyojeong Son <web.sonhyoj@gmail.com>
2017-03-08 17:11:55 +0900

    images folder add, less-loder webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", {loader: 'css-loader', options: {sourceMap: 1}}, "less-loader"]
       }
     ]
   }
